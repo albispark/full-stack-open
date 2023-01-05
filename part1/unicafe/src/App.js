b3f155389304b727e09cb4f9f62a949c6ad565c2
Albi Spahiu <albics@proton.me>
2023-01-05 18:09:53 +0100

    exercise 1.7, 1.8 and 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,12 +14,35 @@ const Stat = (props) => (
     <p>{props.name} {props.stat}</p>
 )
 
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+
+  if (all === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+      <div>
+        <Stat name='good' stat={props.good}/>
+        <Stat name='neutral' stat={props.neutral}/>
+        <Stat name='bad' stat={props.bad}/>
+        <Stat name='all' stat={all}/>
+        <Stat name='average' stat={(props.good - props.bad) / all}/>
+        <Stat name='positive' stat={props.good / all * 100} />
+      </div>
+    )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
   return (
     <div>
       <Header name='give feedback' />
@@ -27,9 +50,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} name='neutral' />
       <Button onClick={() => setBad(bad + 1)} name='bad' />
       <Header name='statistics' />
-      <Stat name='good' stat={good}/>
-      <Stat name='neutral' stat={neutral}/>
-      <Stat name='bad' stat={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
